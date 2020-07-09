a052982b99020cad5350c99ed25f1d67800a4a3f
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-07-09 02:06:54 +0200

    test Afficher list

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const list = [
+  {id: "1", nom: "Shopie", age :"18" },
+  {id: "2", nom: "Julienne", age :"28" },
+  {id: "3", nom: "Marc", age :"30" },
+  {id: "4", nom: "Alexandre", age :"14" },
+]
+
+
+class App extends Component{
+  renderList(){
+    return list.map(item =>{
+      return (
+      <h3 key={item.id}>{item.nom} a {item.age}</h3>
+      )
+    })
+  }
+  render(){
+    return( 
+      <div>
+        <p>Je suis le component ZakApp</p>
+        {this.renderList()}
+      </div>
+    )
+  }
 }
 
 export default App;
